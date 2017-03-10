e20f999082b807c12095b4498b63c609c8e2539f
Yusuke Niitani <yuyuniitani@gmail.com>
2017-03-11 00:20:36 +0900

    bug in pad

--- chainercv/transforms/image/pad_transform.py
+++ chainercv/transforms/image/pad_transform.py
@@ -16,6 +16,8 @@ def pad(x, max_size, bg_value):
     """
     x_slices, y_slices = _get_pad_slices(x, max_size=max_size)
     out = bg_value * np.ones((x.shape[0],) + max_size, dtype=x.dtype)
+    print(y_slices)
+    print(x_slices)
     out[:, y_slices, x_slices] = x
     return out
 
@@ -33,19 +35,19 @@ def _get_pad_slices(img, max_size):
         diff_y = max_size[0] - H
         margin_y = diff_y / 2
         if diff_y % 2 == 0:
-            y_slices = slice(margin_y, max_size[0] - margin_y)
+            y_slices = slice(int(margin_y), int(max_size[0] - margin_y))
         else:
-            y_slices = slice(margin_y, max_size[0] - margin_y - 1)
+            y_slices = slice(int(margin_y), int(max_size[0] - margin_y - 1))
     else:
-        y_slices = slice(0, max_size[0])
+        y_slices = slice(0, int(max_size[0]))
 
     if W < max_size[1]:
         diff_x = max_size[1] - W
         margin_x = diff_x / 2
         if diff_x % 2 == 0:
-            x_slices = slice(margin_x, max_size[1] - margin_x)
+            x_slices = slice(int(margin_x), int(max_size[1] - margin_x))
         else:
-            x_slices = slice(margin_x, max_size[1] - margin_x - 1)
+            x_slices = slice(int(margin_x), int(max_size[1] - margin_x - 1))
     else:
-        x_slices = slice(0, max_size[1])
+        x_slices = slice(0, int(max_size[1]))
     return x_slices, y_slices
